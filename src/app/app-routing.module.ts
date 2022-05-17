d6cb852cdd6f8c394cd543f256639153eca6dfab
Virginia <virgi.96@hotmail.com>
2022-05-17 12:17:14 +0200

    Generated pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+  { path: '',redirectTo: 'home', pathMatch: 'full' },
+  { path: 'movie',loadChildren: () => import('./pages/movie/movie.module').then( m => m.MoviePageModule) },
+  { path: 'home',loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
 ];
 
